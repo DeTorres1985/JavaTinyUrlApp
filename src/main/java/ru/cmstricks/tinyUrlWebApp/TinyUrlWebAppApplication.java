657bbfc5be8e631ee15214a7e3e3671e74342df6
De Torres <detorrez@yandex.ru>
2024-05-26 01:10:21 +0400

    changes in gitignore and initial commit

---
+++ src/main/java/ru/cmstricks/tinyUrlWebApp/TinyUrlWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.cmstricks.tinyUrlWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyUrlWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/cmstricks/tinyUrlWebApp/TinyUrlWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cmstricks.tinyUrlWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyUrlWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
